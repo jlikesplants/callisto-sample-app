9d0e516772b6eefdf80087cf0bf63452357f0736
justine <justine.winnie@gmail.com>
2016-08-19 14:54:37 -0700

    Add callisto-core urls.

--- callisto_sample_app/callisto_sample_app/urls.py
+++ callisto_sample_app/callisto_sample_app/urls.py
@@ -1,21 +1,54 @@
-"""callisto_sample_app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
+from callisto.delivery.views import (
+    delete_report, edit_record_form_view, export_as_pdf, new_record_form_view,
+    submit_to_matching, submit_to_school, withdraw_from_matching,
+)
+
+from callisto_sample_project.core.forms import (
+    CustomMatchReport, CustomReport, EncryptedFormWizard,
+)
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+
+    # Django Admin, use {% url 'admin:index' %}
+    url(settings.ADMIN_URL, include(admin.site.urls)),
+
+    # User management
+    url(r'^users/', include('callisto_sample_project.users.urls', namespace='users')),
+    url(r'^accounts/', include('allauth.urls')),
+
+    # Callisto core views
+    url(r'^reports/new/(?P<step>.+)/$', new_record_form_view,
+        {'wizard': EncryptedFormWizard,
+         'url_name': 'test_new_report'}, name="test_new_report"),
+    url(r'^reports/edit/(?P<edit_id>\d+)/$', edit_record_form_view,
+        {'wizard': EncryptedFormWizard, 'url_name': 'test_edit_report'}, name='test_edit_report'),
+    url(r'^reports/edit/(?P<edit_id>\d+)/(?P<step>.+)/$', edit_record_form_view,
+        {'wizard': EncryptedFormWizard, 'url_name': 'test_edit_report'}, name='test_edit_report'),
+    url(r'^reports/submit/(?P<report_id>\d+)/$', submit_to_school, name="test_submit_report"),
+    url(r'^reports/submit_custom/(?P<report_id>\d+)/$', submit_to_school,
+        {'form_template_name': 'submit_to_school_custom.html',
+         'confirmation_template_name': 'submit_to_school_confirmation_custom.html',
+         'report_class': CustomReport,
+         'extra_context': {'test': 'custom context'}}, name="test_submit_confirmation"),
+    url(r'^reports/match/(?P<report_id>\d+)/$', submit_to_matching, name="test_submit_match"),
+    url(r'^reports/match_custom/(?P<report_id>\d+)/$', submit_to_matching,
+        {'form_template_name': 'submit_to_matching_custom.html',
+         'confirmation_template_name': 'submit_to_matching_confirmation_custom.html',
+         'report_class': CustomMatchReport,
+         'extra_context': {'test': 'custom context'}}, name="test_match_confirmation"),
+    url(r'^reports/withdraw_match/(?P<report_id>\d+)/$', withdraw_from_matching,
+        {'template_name': 'after_withdraw.html',
+         'extra_context': {'test': 'custom context'}}, name="test_withdraw_match"),
+    url(r'^reports/export/(?P<report_id>\d+)/$', export_as_pdf, name="test_export"),
+    url(r'^reports/export_custom/(?P<report_id>\d+)/$', export_as_pdf,
+        {'report_class': CustomReport,
+         'extra_context': {'test': 'custom context'}}, name="test_export_custom"),
+    url(r'^reports/delete/(?P<report_id>\d+)/$', delete_report,
+        {'extra_context': {'test': 'custom context'}}, name="delete_report")
+
 ]
